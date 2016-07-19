a07a1e30e65627e4b53925937b6550e6d13ad940
Yuki <lalasax20999@gmail.com>
2016-07-20 03:11:13 +0700

    Update
    ver 1.2

---
+++ MeasuringTimeSpentClock/MeasuringTimeSpentClock/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace MeasuringTimeSpentClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.acummulatedTime_label = new System.Windows.Forms.Label();
+            this.timeCount_label = new System.Windows.Forms.Label();
+            this.timeString = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnAbout = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(12, 19);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(113, 13);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Current time goes here";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(114, 95);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(62, 52);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(197, 95);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(65, 52);
+            this.BtnStop.TabIndex = 2;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(279, 95);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(72, 52);
+            this.BtnRefresh.TabIndex = 3;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // acummulatedTime_label
+            // 
+            this.acummulatedTime_label.AutoSize = true;
+            this.acummulatedTime_label.Location = new System.Drawing.Point(35, 166);
+            this.acummulatedTime_label.Name = "acummulatedTime_label";
+            this.acummulatedTime_label.Size = new System.Drawing.Size(123, 13);
+            this.acummulatedTime_label.TabIndex = 4;
+            this.acummulatedTime_label.Text = "Total accumulated time :";
+            // 
+            // timeCount_label
+            // 
+            this.timeCount_label.AutoSize = true;
+            this.timeCount_label.Location = new System.Drawing.Point(35, 183);
+            this.timeCount_label.Name = "timeCount_label";
+            this.timeCount_label.Size = new System.Drawing.Size(141, 13);
+            this.timeCount_label.TabIndex = 5;
+            this.timeCount_label.Text = "Accumulated time goes here";
+            // 
+            // timeString
+            // 
+            this.timeString.AutoSize = true;
+            this.timeString.Location = new System.Drawing.Point(35, 200);
+            this.timeString.Name = "timeString";
+            this.timeString.Size = new System.Drawing.Size(169, 13);
+            this.timeString.TabIndex = 6;
+            this.timeString.Text = "Accumulated time in formated form";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hot keys : Start(A)  Stop(S)  Refresh(D) Minimize(M)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Started time :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(229, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Time started";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Stopped time :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(229, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Time stopped";
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(142, 266);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(158, 45);
+            this.BtnExit.TabIndex = 14;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // BtnAbout
+            // 
+            this.BtnAbout.Location = new System.Drawing.Point(373, 287);
+            this.BtnAbout.Name = "BtnAbout";
+            this.BtnAbout.Size = new System.Drawing.Size(75, 23);
+            this.BtnAbout.TabIndex = 15;
+            this.BtnAbout.Text = "About";
+            this.BtnAbout.UseVisualStyleBackColor = true;
+            this.BtnAbout.Click += new System.EventHandler(this.BtnAbout_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(117, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Set your Interval here : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 323);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.BtnAbout);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeString);
+            this.Controls.Add(this.timeCount_label);
+            this.Controls.Add(this.acummulatedTime_label);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.timeLabel);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Measuring Time Spent Clock v1.1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.Label acummulatedTime_label;
+        private System.Windows.Forms.Label timeCount_label;
+        private System.Windows.Forms.Label timeString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnAbout;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MeasuringTimeSpentClock/MeasuringTimeSpentClock/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MeasuringTimeSpentClock
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 100; // set interval
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+            timeLabel.Text = DateTime.Now.ToString("F");
+            timeLabel.Anchor = (AnchorStyles)(AnchorStyles.Left) | (AnchorStyles.Right);
+            timeLabel.AutoSize = false;
+            //timeLabel.Dock = DockStyle.Fill;
+            timeLabel.TextAlign = ContentAlignment.MiddleCenter;
+            //MessageBox.Show(DateTime.Now.ToString("mm, yyyy hh:mm:ss"));
+            label5.Text = DateTime.Now.ToString("F");
+            this.Opacity = .83;
+        }
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                timeLabel.Text = DateTime.Now.ToString("F"); //mm, yyyy hh:mm:ss
+                Refresh();
+            //if (isStarted)
+            {
+                TimeSpan elapsedTime = sw.Elapsed;
+                timeCount_label.Text = elapsedTime.ToString();
+                string finalString = "";
+                finalString = finalString + elapsedTime.Hours + " Hours " +
+                    elapsedTime.Minutes + " Minutes " + elapsedTime.Seconds +  " Seconds " +
+                    elapsedTime.Milliseconds + " Miliseconds ";
+                timeString.Text = finalString;
+                if (!minOnce && sw.Elapsed.Seconds == 3) // Auto minimize after 3 seconds once, for lazy people 
+                {
+                    try
+                    {
+                        Form1.ActiveForm.WindowState = FormWindowState.Minimized; 
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    minOnce = true;
+                }
+            }
+            }
+        Stopwatch sw = Stopwatch.StartNew();
+        bool isStopped = false;
+        bool minOnce = false;
+        //bool isStarted = false;
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //isStarted = true;
+            if (!isStopped)
+            {
+                sw = Stopwatch.StartNew();
+                isStopped = false;
+            }
+            else
+            {
+                sw.Start();
+            }
+            label2.Text = "";
+            label1.Text = "Started ! ";
+            label5.Text = DateTime.Now.ToString("F");
+
+        }
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+            isStopped = true;
+            //isStarted = false;
+            label1.Text = "";
+            label2.Text = "Stopped !";
+            label7.Text = DateTime.Now.ToString("F");
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            sw.Reset();
+            label5.Text = "";
+            label7.Text = "";
+        }
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made by Yuki in his free time");
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                timer.Interval = int.Parse(textBox1.Text);
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // Doesn't work yet
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.M: // M for minimizing 
+                    this.WindowState = FormWindowState.Minimized;
+                    break;
+                case Keys.A: // A for "Start"
+                    BtnStart_Click(sender, e);
+                    break;
+                case Keys.S: // S for "Stop"
+                    BtnStop_Click(sender,e);
+                    break;
+                case Keys.D: // D for "Refresh"
+                    BtnRefresh_Click(sender, e);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
